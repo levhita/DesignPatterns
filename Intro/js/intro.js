786d91d57b4d045802400326c48c73d7637ceff9
Levhita Arias <levhita@gmail.com>
2023-06-20 19:35:29 -0600

    arrow

--- Intro/js/intro.js
+++ Intro/js/intro.js
@@ -1,5 +1,16 @@
+// First Order
 function sum (a, b) {
   return a + b
 }
+//  Higer Order
+function operation (fn, a, b) {
+  return fn(a, b)
+}
 
-console.log(sum(2, 3))
+console.log(operation(sum, 5, 6))
+
+// Arrow
+const a = () => console.log('something')
+a()
+
+console.log(operation((a, b) => a - b, 5, 5))
--- Intro/ts/intro.ts
+++ Intro/ts/intro.ts
@@ -3,3 +3,16 @@ function sum (a: number, b: number): number {
 }
 
 console.log(sum(2, 3))
+
+type operationFunction = (a: number, b: number) => number
+function operation (fn: operationFunction, a: number, b: number): number {
+  return fn(a, b)
+}
+
+console.log(operation(sum, 5, 6))
+
+const a = (): void => { console.log('something') }
+
+a()
+
+console.log(operation((a, b) => a + b, 5, 5))
